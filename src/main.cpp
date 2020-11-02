75b66305797588f785d147d26287f75694a99100
sjrdc <scrijns@gmail.com>
2020-11-02 20:22:35 +0100

    Test parsing floating point value

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,21 @@ TEST(Pax, CanParseIntegralValueArg)
     EXPECT_EQ(5, arg.get_value());
 }
 
+TEST(Pax, CanParseFloatingPointValueArg)
+{
+    pax::command_line cmd("cmd");
+
+    auto& arg = cmd.add_value_argument<float>("some integer")
+	.set_tag("-f")
+	.set_long_tag("--float");
+
+    constexpr auto f = 1.23f;
+    std::vector<std::string> args = {"piet", "-f", std::to_string(f)};
+    cmd.parse(args);
+
+    EXPECT_EQ(f, arg.get_value());
+}
+
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
